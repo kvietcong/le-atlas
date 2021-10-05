83042b81c5fea157f814dd39673478a7017296e4
KV Le <32186663+kvietcong@users.noreply.github.com>
2021-10-04 17:31:06 -0700

    🛠️Fix Fonts

--- pages/_document.js
+++ pages/_document.js
@@ -13,7 +13,7 @@ class MyDocument extends Document {
         <Head>
           {/* Merriweather Font */}
           <link rel="preconnect" href="https://fonts.googleapis.com"/>
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet"/>
         </Head>
         <body className="dark-mode">
