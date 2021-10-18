1efb2eb0b13bf6a54f69ea0172546ccf6f57d0d1
KV Le <32186663+kvietcong@users.noreply.github.com>
2021-10-18 15:12:58 -0700

    🪄 Improve Search Functionality

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,12 @@ import Head from "next/head";
 import Link from "next/link";
 import { useState } from "react";
 import { notes } from "../utils/atlasManagement";
+import { noteSearch } from "../utils/general";
 
 export default function Home({ notes }) {
   const [ search, setSearch ] = useState("");
   return (
-    <main>
+    <main id="home" style={{width: "50%", margin: "auto"}}>
       <Head>
         <title>Le Atlas</title>
       </Head>
@@ -14,12 +15,14 @@ export default function Home({ notes }) {
         <h1>
           Welcome to Le Atlas!
         </h1>
+        <p>Just a place to display my Digital Garden (notes) and stuff</p>
+        <p>Built with Next.js and the UnifiedJS Parsing Ecosystem</p>
         <h2>Links</h2>
         <ul>
           <li>
             <h3>Graphs</h3>
             <ul>
-              <li><Link href="/atlas/graph/2D">2D Graph</Link></li>
+              <li><Link href="/atlas/graph/2D">2D Graph (Not yet)</Link></li>
               <li><Link href="/atlas/graph/3D">3D Graph</Link></li>
             </ul>
           </li>
@@ -30,6 +33,14 @@ export default function Home({ notes }) {
         </ul>
         <section>
           <h2>Search</h2>
+          <p>
+            This search is a bit special. It splits up your search into multiple
+            RegExes and scores it based on the matching parts. Inspired by
+            the <a
+              href="https://github.com/oantolin/orderless"
+              target="_blank" rel="noreferrer"
+            >Orderless</a> Emacs plugin (Though not even close in implementation)
+          </p>
           <input
             type="text"
             onChange={evt => setSearch(evt.target.value)}
@@ -37,19 +48,14 @@ export default function Home({ notes }) {
             placeholder="Search for a Note"
           />
           <ul>
-            {Object.entries(notes)
-              .filter(([_, note]) =>
-                search.length === 0
-                || note.title.toLowerCase().includes(search.toLowerCase())
-              )
+            {noteSearch(search, Object.entries(notes))
               .map(([slug, note]) =>
                 <li key={slug}>
                   <Link href={`/atlas/?notes=${slug}`}>
                     {note.title}
                   </Link>
                 </li>
-              )
-            }
+              )}
           </ul>
         </section>
       </main>
--- utils/general.js
+++ utils/general.js
@@ -8,3 +8,26 @@ export const time = (...args) => {
             [ 0, 0 ]
         )[0];
 };
+
+export const noteSearch = (search, notes) => {
+    if (!search) return notes;
+
+    const scores = notes.reduce((accumulated, [slug, note]) => {
+        const toSearch = note.title.toLowerCase();
+        const searchTerms = search.toLowerCase().split(" ");
+        const regexString = `(${searchTerms.join("|")})`;
+        const matches = [...new Set(toSearch.match(new RegExp(regexString, "g")))];
+        matches.sort((a, b) => b.length - a.length);
+        accumulated[slug] = matches.reduce(
+            (accumulated, item, i) => item.length / (i+1) + accumulated, 0);
+        return accumulated;
+    }, {});
+    console.log(scores)
+
+    const result = notes.filter(([slug, _]) => scores[slug])
+    result.sort(([a, _a], [b, _b]) => {
+        const scoring = scores[b] - scores[a];
+        return (scoring === 0) ? a.length - b.length : scoring;
+    });
+    return result;
+}
